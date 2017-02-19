4f97714d28e41ae6ec8daa48db012db80488b2f1
Pekka Lukkari <pekka.lukkari@github.com>
2017-02-19 23:52:37 +0200

    Aloitettu demo.

---
+++ Ajanvarausdemo/src/Ajanvarausdemo/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Write your Javascript code.
+
+$(document).ready(function () {
+
+    //flatpickr(".flatpickr");
+
+    flatpickr(".flatpickr", {
+        inline: true, // show the calendar inline
+        weekNumbers: true, // show week numbers
+        locale: 'fi'
+    })
+});
